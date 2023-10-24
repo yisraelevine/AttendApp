e6cabfbbfbadb2351e12ca4b35bc74168229d65e
yisraelevine <yisraelevine@gmail.com>
2023-10-24 01:44:02 -0400

    Add interfaces

--- src/app/global.service.ts
+++ src/app/global.service.ts
@@ -2,16 +2,25 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { getDate } from './get-date';
 interface classesListInterface {
-	isAdmin: boolean;
+	isAdmin: boolean,
 	list: {
-		id: number;
-		name: string;
-	}[];
-}
+		id: number,
+		name: string
+	}[]
+};
 interface permissionsListInterface {
-	id: number;
-	email: string;
-}
+	id: number,
+	email: string
+};
+interface studentsListInterface {
+	id: number,
+	last_name: string,
+	first_name: string,
+	visible: boolean,
+	arrived: boolean | null,
+	time_in: string | null,
+	time_out: string | null
+};
 @Injectable({
 	providedIn: 'root'
 })
@@ -21,11 +30,10 @@ export class GlobalService {
 
 	selectedClassName = "";
 	selectedStudent = -1;
-	classesList: any[] = [];
-	studentsList: any[] = [];
-	permissionsList: any[] = [];
+	classesList: classesListInterface = { isAdmin: false, list: [] };
+	studentsList: studentsListInterface[] = [];
+	permissionsList: permissionsListInterface[] = [];
 	componentShown = -1;
-	isAdmin = false;
 
 	getClasses() {
 		this.http.get<classesListInterface>(
@@ -34,35 +42,14 @@ export class GlobalService {
 				responseType: 'json'
 			}
 		).subscribe({
-			next: (data) => {
-				this.classesList = data.list;
-				this.isAdmin = data.isAdmin;
-			},
+			next: (data) => this.classesList = data,
 			error: () => this.componentShown = 0,
 			complete: () => this.componentShown = 0
 		});
 	}
 
-	getPermissions(class_id: number) {
-		this.http.post<any>(
-			'/data/permissions',
-			{
-				class_id: class_id
-			},
-			{
-				responseType: 'json'
-			}
-		).subscribe({
-			next: (data: any) => {
-				this.permissionsList = data;
-			},
-			error: () => this.componentShown = 2,
-			complete: () => this.componentShown = 2
-		});
-	}
-
 	getStudents(class_id: number) {
-		this.http.post<any>(
+		this.http.post<studentsListInterface[]>(
 			'/data/students',
 			{
 				date: new getDate().jdate,
@@ -72,11 +59,27 @@ export class GlobalService {
 				responseType: 'json'
 			}
 		).subscribe({
-			next: (data: any) => this.studentsList = data,
+			next: (data) => this.studentsList = data,
 			error: () => this.componentShown = 1,
 			complete: () => this.componentShown = 1
 		})
 
+	}
+
+	getPermissions(class_id: number) {
+		this.http.post<permissionsListInterface[]>(
+			'/data/permissions',
+			{
+				class_id: class_id
+			},
+			{
+				responseType: 'json'
+			}
+		).subscribe({
+			next: (data) => this.permissionsList = data,
+			error: () => this.componentShown = 2,
+			complete: () => this.componentShown = 2
+		});
 	}
 
 	upsertStudent(
