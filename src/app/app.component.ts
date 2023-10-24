bd97c00df0b1f1ec5a5cbcdd449bf109a38c65f4
yisraelevine <yisraelevine@gmail.com>
2023-10-24 02:09:37 -0400

    Add fade animation to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { GlobalService } from './global.service';
+import { fade } from './animations';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
+	styleUrls: ['./app.component.css'],
+	animations: [fade]
 })
 export class AppComponent implements OnInit {
 	constructor(public globalService: GlobalService) { }
