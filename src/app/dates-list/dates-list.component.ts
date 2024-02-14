d7de35afef1af585a02e646c372900cfd50ed133
yisraelevine <yisraelevine@gmail.com>
2024-02-14 06:15:06 -0500

    Continuing feature development: Adding hebrew calander, calander changing and optimizing

--- src/app/dates-list/dates-list.component.ts
+++ src/app/dates-list/dates-list.component.ts
@@ -10,45 +10,11 @@ import { GlobalService } from '../global.service'
 })
 export class DatesListComponent {
 	animationState = ''
-	selected = ''
-	timeout: any
+	hebrew: boolean = false
 	constructor(public service: GlobalService) { }
 	backEvent() {
 		this.animationState = 'void'
 		this.service.getStudents()
-	}
-	clockEvent(date: string) { this.selected = this.selected === date ? '' : date }
-	checkboxEvent(date: string) {
-		for (const item of this.service.pastRecords!.records) {
-			if (item.date.date !== date) continue
-			if (item.time_in === null && item.time_out === null) this.service.upsertStudent(
-				date, this.service.selectedStudent.id, item.arrived = !item.arrived, item.time_in, item.time_out, item.text)
-			else this.selected = date
-			break
-		}
-	}
-	timeEvent(date: string, time: string | null, isTimeIn: boolean) {
-		for (const item of this.service.pastRecords!.records) {
-			if (item.date.date !== date) continue
-			time = time?.length === 0 ? null : time
-			isTimeIn ? item.time_in = time : item.time_out = time
-			if (time) item.arrived = true
-			clearTimeout(this.timeout)
-			this.timeout = setTimeout(() => this.service.upsertStudent(
-				date, this.service.selectedStudent.id, item.arrived, item.time_in, item.time_out, item.text), 200)
-			break
-		}
-	}
-	textEvent(date: string, text: string | null) {
-		for (const item of this.service.pastRecords!.records) {
-			if (item.date.date !== date) continue
-			text = text?.length === 0 ? null : text
-			item.text = text
-			clearTimeout(this.timeout)
-			this.timeout = setTimeout(() => this.service.upsertStudent(
-				date, this.service.selectedStudent.id, item.arrived, item.time_in, item.time_out, item.text), 200)
-			break
-		}
 	}
 	isHidden = (date: string) => {
 		const _date = new Date(date)
--- src/app/past-records.ts
+++ src/app/past-records.ts
@@ -1,3 +1,4 @@
+import { HDate } from "@hebcal/core";
 import { AttendanceRecord } from "./interfaces";
 
 interface Record {
@@ -5,10 +6,14 @@ interface Record {
         date: string
         date_number: number
         day_number: number
-        month_name: string
         month_number: number
         year_number: number
         week_number: number
+
+        hdate_string: string
+        hmonth_number: number
+        hyear_number: number
+        hweek_number: number
     }
     arrived: boolean | null
     time_in: string | null
@@ -18,11 +23,14 @@ interface Record {
 
 export class PastRecords {
     public daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+    public daysOfHWeek = ["ראשון", "שני", "שלישי", "רביעי", "חמישי", "שישי", "שבת"]
     public records: Record[] = []
     public recordsByMonth: { name: string, records: Record[] }[]
+    public recordsByHMonth: { name: string, records: Record[] }[]
     constructor(records: AttendanceRecord[], registration_date: string | null) {
         this.records = this.generateRecords(records, registration_date)
         this.recordsByMonth = this.groupByMonth()
+        this.recordsByHMonth = this.groupByHMonth()
     }
     private getMonths(): { m: number, y: number }[] {
         return [...new Set(
@@ -38,19 +46,37 @@ export class PastRecords {
             records: this.getRecordsByMonth(e.m, e.y)
         }))
     }
+    private getHMonths(): { m: number, y: number }[] {
+        return [...new Set(
+            this.records.map(e => JSON.stringify({ m: e.date.hmonth_number, y: e.date.hyear_number }))
+        )].map(e => JSON.parse(e))
+    }
+    private getRecordsByHMonth(month: number, year: number) {
+        return this.records.filter(e => e.date.hmonth_number === month && e.date.hyear_number === year)
+    }
+    private groupByHMonth(): { name: string, records: Record[] }[] {
+        return this.getHMonths().map(e => ({
+            name: new HDate(1, e.m, e.y).renderGematriya(true).split(' ').slice(1).join(' '),
+            records: this.getRecordsByHMonth(e.m, e.y)
+        }))
+    }
     private isLastDayOfMonth(date: Date): boolean {
-        return date.getDate() === new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+        return date.getDate() === new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate()
+    }
+    private isLastDayOfHMonth(date: HDate): boolean {
+        return date.getDate() === date.daysInMonth()
     }
     private generateRecords(records: AttendanceRecord[], registration_date: string | null): Record[] {
         records.forEach(e => e.date = new Date(e.date.slice(0, -1)).toDateString())
         const end_date = new Date()
         end_date.setHours(end_date.getHours() - 4, 0, 0, 0)
         const start_date = new Date(registration_date?.slice(0, -1) || end_date)
-        start_date.setDate(start_date.getDate() - start_date.getDay())
         const result: Record[] = []
-        for (let is_first = true, week = 0; end_date.getTime() >= start_date.getTime(); end_date.setDate(end_date.getDate() - 1), is_first = false) {
+        for (let is_first = true, week = 0, hweek = 0; end_date.getTime() >= start_date.getTime(); end_date.setDate(end_date.getDate() - 1), is_first = false) {
+            const hdate = new HDate(end_date)
+            if (end_date.getDay() === 6 && !is_first) week++, hweek++
             if (this.isLastDayOfMonth(end_date)) week = 0
-            if (end_date.getDay() === 6 && !is_first) week++
+            if (this.isLastDayOfHMonth(hdate)) hweek = 0
             const record = records.find(e => e.date === end_date.toDateString())
             result.push({
                 date: {
@@ -58,9 +84,12 @@ export class PastRecords {
                     day_number: end_date.getDay(),
                     date_number: end_date.getDate(),
                     month_number: end_date.getMonth(),
-                    month_name: end_date.toLocaleString('default', { month: 'long' }),
                     year_number: end_date.getFullYear(),
-                    week_number: week
+                    week_number: week,
+                    hdate_string: hdate.renderGematriya(true).split(' ')[0].replace(/[^א-ת]/g, ''),
+                    hmonth_number: hdate.getMonth(),
+                    hyear_number: hdate.getFullYear(),
+                    hweek_number: hweek
                 },
                 arrived: record?.arrived || null,
                 time_in: record?.time_in || null,
