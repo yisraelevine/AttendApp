6efb15e7e74fc5c48a17e312388b5e253384cada
yisrael Levine <yisraelevine@gmail.com>
2024-09-13 05:32:00 -0400

    Clearing

--- android/app/src/main/java/com/acmschool/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.acmschool.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.acmschool.app',
-  appName: 'attend-app',
-  webDir: '../AttendServer/app'
-};
-
-export default config;
